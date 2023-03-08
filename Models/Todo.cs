996765fe36fc59e57f185bf30a68ca5685d60319
Igor Pawlowski <igorpawlowski11@gmail.com>
2023-03-08 11:55:13 +0100

    added a 'todo' object model

---
+++ Models/Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Postgrest.Attributes;
+using Postgrest.Models;
+using Supabase;
+
+namespace Todos.Models
+{
+    [Table("todos")]
+    public class Todo : BaseModel
+    {
+        [PrimaryKey("id", false)]
+        public int Id { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set;}
+
+        [Column("todo")]
+        public string? Text { get; set; }
+    }
+}
