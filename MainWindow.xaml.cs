88c5cadfecde7707376b83e6855618d787cfc374
Igor Pawlowski <igorpawlowski11@gmail.com>
2023-03-13 10:07:22 +0100

    visual adjustments; added passing url as credential

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Todos.Models;
 using Todos;
 using Todos.Properties;
 using System.Collections.ObjectModel;
+using Microsoft.Extensions.Configuration;
 
 namespace Todos
 {
@@ -20,10 +21,10 @@ namespace Todos
     /// </summary>
     public partial class MainWindow : Window
     {
-        private String url = "https://tcgqpbvdhirptzglzlrj.supabase.co";
-        private Supabase.Client client;
-        private String _key;
-        private ObservableCollection<String> _todos = new ObservableCollection<String>();
+        private static Supabase.Client client;
+        private static String _url = Settings.Default.url;
+        private static String _key = Settings.Default.apikey;
+        private static ObservableCollection<String> _todos = new ObservableCollection<String>();
 
 
         public MainWindow()
@@ -35,7 +36,7 @@ namespace Todos
         {
             if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstTodos.Items.Contains(txtName.Text)) 
             { 
-                lstTodos.Items.Add(txtName.Text);
+                _todos.Add(txtName.Text);
                 await client.From<Models.Todo>().Insert(new Todo { Text = txtName.Text });  
                 txtName.Clear();
                 FetchTodos();
@@ -47,6 +48,7 @@ namespace Todos
             try
             {
                 _key = Settings.Default.apikey;
+                _url = Settings.Default.url;
             } 
             catch (ArgumentNullException ex) 
             {
@@ -54,11 +56,12 @@ namespace Todos
                 modalWindow.ShowDialog();
 
                 _key = ApiKeyDialogWindow.myApikey;
+                _url = ApiKeyDialogWindow.myUrl;
+                Settings.Default.url = _url;
                 Settings.Default.apikey = _key;
             }
 
         }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             lstTodos.ItemsSource = _todos;
@@ -68,11 +71,28 @@ namespace Todos
                 modalWindow.ShowDialog();
 
                 _key = ApiKeyDialogWindow.myApikey;
+                _url = ApiKeyDialogWindow.myUrl;
                 Settings.Default.apikey = _key;
+                Settings.Default.url = _url;
             }
             else
             {
                 _key = Settings.Default.apikey;
+            }
+
+            if (String.IsNullOrEmpty(Settings.Default.url))
+            {
+                ApiKeyDialogWindow modalWindow = new ApiKeyDialogWindow();
+                modalWindow.ShowDialog();
+
+                _key = ApiKeyDialogWindow.myApikey;
+                _url = ApiKeyDialogWindow.myUrl;
+                Settings.Default.apikey = _key;
+                Settings.Default.url = _url;
+            }
+            else
+            {
+                _url = Settings.Default.url;
             }
 
             InitSupabase();
@@ -82,24 +102,30 @@ namespace Todos
 
         private async void InitSupabase()
         {
+            var builder = new ConfigurationBuilder();
             var options = new SupabaseOptions { AutoConnectRealtime = true };
-            client = new Supabase.Client(url, _key, options);
+            client = new Supabase.Client(_url, _key, options);
             await client.InitializeAsync();
-
-
         }
 
-        private async void FetchTodos()
+        public static async void FetchTodos()
         {
-            var query = await client.From<Models.Todo>().Get();
-
-            foreach (var todo in query.Models)
+            try
             {
-                if (!_todos.Contains(todo.Text))
+                var query = await client.From<Models.Todo>().Get();
+
+                foreach (var todo in query.Models)
                 {
-                    _todos.Add(todo.Text);
-                }
-            };
+                    if (!_todos.Contains(todo.Text))
+                    {
+                        _todos.Add(todo.Text);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not fetch data, please try again.", "Error", MessageBoxButton.OKCancel, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
         }
 
         private void btnTest_Click(object sender, RoutedEventArgs e)
@@ -114,6 +140,7 @@ namespace Todos
         private void Window_Closed(object sender, EventArgs e)
         {
             Properties.Settings.Default.apikey = _key;
+            Properties.Settings.Default.url = _url;
             Properties.Settings.Default.Save();
         }
 
@@ -123,14 +150,10 @@ namespace Todos
             if (button != null)
             {
                 var todoText = button.DataContext.ToString();
-                Trace.WriteLine(todoText);
-
                 await client.From<Models.Todo>().Where(x => x.Text == todoText).Delete();
                 _todos.Remove(todoText);
             }
-
             FetchTodos();
-            Trace.WriteLine("todos fetched again");
         }
     }
 }
